70470c7fb78fc92e9abbb34f97b043511d43e986
BakariF <bakkfofhit@live.fr>
2021-01-22 09:44:44 +0100

    gneu

---
+++ La quizz super sérieux de C_SHARP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace La_quizz_super_sérieux_de_C_SHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
